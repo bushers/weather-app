2afdbfadf2de466c221da3c792f37d07143ca2d9
Nick Bushby <nbushby01@hotmail.com>
2017-07-04 21:57:26 +0100

    add Input component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Input from './Input'
+
+const apiKey = 'eaf18ed4081c3113e6f5ab8081991e98'
 
 const App = () => (
   <div className='container' style={{textAlign: 'center'}}>
-    <div></div>
+    <div>Hello World</div>
+    <Input />
   </div>
 )
 
---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Input = () => (
+  <div>
+    <input type='text' placeholder='Enter a city name' />
+    <input type='submit' value='Click' />
+  </div>
+)
+
+export default Input
