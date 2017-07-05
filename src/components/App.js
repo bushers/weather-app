46ddeee0c3e26d9f3d76c083f6f07c33efad615a
Nick Bushby <nbushby01@hotmail.com>
2017-07-05 21:25:53 +0100

    add fetch method for forcast data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ForcastDisplay from './ForcastDisplay'
 import Input from './Input'
 
-const apiKey = 'eaf18ed4081c3113e6f5ab8081991e98'
+class App extends Component {
+  state = {
+    forcastType: null,
+    city: 'Leeds',
+    forcastData: null
+  }
 
-const App = () => (
-  <div className='container' style={{textAlign: 'center'}}>
-    <div>Hello World</div>
-    <Input />
-  </div>
-)
+  getForcast = () => {
+    const apiKey = 'eaf18ed4081c3113e6f5ab8081991e98'
+    const apiCall = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${apiKey}`
+
+    window.fetch(apiCall)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ forcastData: data })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+     <div className='container' style={{textAlign: 'center'}}>
+       <div>Hello World</div>
+       <ForcastDisplay />
+       <Input />
+     </div>
+   )
+  }
+}
 
 export default App
---
+++ src/components/ForcastDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ForcastDisplay = (props) => (
+  <div>
+    <button>Current Forcast</button>
+    <button>5 Day Forcast</button>
+  </div>
+)
+
+export default ForcastDisplay
